3a31194f3a5f0f06ced92b676f56b995bd254550
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-02 18:25:46 +0100

    folders and global styles added

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,13 @@ import styles from "./page.module.scss";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.container}>responsive</div>
+      <div className={styles.container}>
+        <h1>responsive</h1>
+        <li>
+          <a href="#">link</a>
+          <a>link 2</a>
+        </li>
+      </div>
     </main>
   );
 }

8f97be266d94598809f7caf54632ec96f431302a
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-03 15:05:51 +0100

    responsive carousel added

--- src/app/components/Carousel/Carousel.jsx
+++ src/app/components/Carousel/Carousel.jsx
@@ -1,18 +1,32 @@
+import styles from './Carousel.module.scss'
+import Button from '../Button/Button.jsx'
 
 export default function Carousel() {
   return (
         <div id="carouselExampleIndicators" className="carousel slide">
-            <div className="carousel-indicators">
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            <div className="carousel-indicators" style={{ position: 'absolute', right: '5%', left: 'auto', bottom: '4%', margin: '10px' }}>
+                <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" className="active" style={{height:'24px', width:'24px', borderRadius:'50%', border:'3px solid #ffc67e', backgroundColor: 'rgba(255, 198, 126, 1)'}} aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" style={{height:'24px', width:'24px', borderRadius:'50%', border:'3px solid #fff', backgroundColor: '#ffc67e', marginLeft:'1.5rem'}} data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleIndicators" style={{height:'24px', width:'24px', borderRadius:'50%', border:'3px solid #fff', backgroundColor: '#ffc67e', marginLeft:'1.5rem'}} data-bs-slide-to="2" aria-label="Slide 3"></button>
             </div>
-            <div className="carousel-inner">
+            <div className={`carousel-inner ${styles.slide}`}>
                 <div className="carousel-item active">
-                    <img src="/carousel/carousel-1.jpeg" className="d-block w-100" alt="..." objectFit="cover" />
+                    <img src="/carousel/carousel-1.jpeg" className={`d-block w-100 ${styles.carouselImage}`} alt="los doctores saliendo del tardis"  />
+                    <div className={styles.overlayContent}>
+                        <h1 className={styles.legend}>The Giggle</h1>
+                        <div className={styles.buttonCarousel}>
+                            <Button href="https://www.doctorwho.tv/">Ver ahora</Button>
+                        </div>
+                    </div>
                 </div>
                 <div className="carousel-item">
-                    <img src="/carousel/carousel-2.jpeg" className="d-block w-100" alt="..." objectFit="cover" />
+                    <img src="/carousel/carousel-2.jpeg" className={`d-block w-100 ${styles.carouselImage}`} alt="dentro del tardis" />
+                    <div className={styles.overlayContent}>
+                        <h1 className={styles.legend}>Allons-y</h1>
+                        <div className={styles.buttonCarousel}>
+                            <Button href="https://www.doctorwho.tv/">Ver ahora</Button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>

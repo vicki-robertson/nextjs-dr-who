d514a52e59b457e3cadd16115f565e4711843033
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-03 19:51:41 +0100

    doctors cards added with responsive styling

--- src/app/components/DoctorsCard/DoctorsCard.jsx
+++ src/app/components/DoctorsCard/DoctorsCard.jsx
@@ -6,19 +6,19 @@ import SmallButton from '../Button/SmallButton';
 export default function DoctorsCard() {
     let cardArr = doctorsList;
     return (
-        <div className='row'>
+        <div className={`row ${styles.row}`}>
         {cardArr.doctors.map((eachCard, id)=>(
-            <div className='col-md-3 g-5' key={eachCard.id}>    
+            <div className={`col-xl-3 col-lg-4 col-md-6 col-sm-6 g-5 ${styles.grid}`} key={eachCard.id}>    
                 <div className={styles.card}>
-                    <img src={eachCard.image} className="card-img-top img-fluid" alt="..." />
+                    <img src={eachCard.image} className="card-img-top img-fluid" alt="fotos de los doctores" style={{height:'460px', objectFit: 'cover'}}/>
                     <div className={styles.cardBody}>
                         <h3>{eachCard.doctor}</h3>
                         <h4>{eachCard.actor}</h4>
                         <p>{eachCard.screen_time}</p>
-                        <div>
-                            <SmallButton href="https://www.doctorwho.tv/">Ver más</SmallButton>
-                            </div>
                     </div>
+                    <SmallButton href="https://www.doctorwho.tv/">
+                            Ver más
+                    </SmallButton>
                 </div>
             </div>
             ))}

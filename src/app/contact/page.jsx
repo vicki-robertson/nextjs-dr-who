6d34459c57e6770f11a6db51720d0a32075169ea
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-04 21:59:40 +0100

    contact form finished desktop size

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,20 +5,22 @@ import Symbols from '../components/Symbols/Symbols';
 export default function contactPage () {
 
   return (
-    <section className={styles.container}>
-        <div className={styles.title}>
-            <h2>Envia tu mensaje al Doctor</h2>
-        </div>
-        <div className={styles.formSection}>
-            <div className={styles.formBox}>
-              <div className={styles.form}>
-                <Form />
+    <div className={styles.main}>
+      <section className={styles.container}>
+          <div className={styles.title}>
+              <h2>Envia tu mensaje al Doctor</h2>
+          </div>
+          <div className={styles.formSection}>
+              <div className={styles.formBox}>
+                <div className={styles.form}>
+                  <Form />
+                </div>
+                <div className={styles.symbolsBox}>
+                  <Symbols />
+                </div>
               </div>
-              <div className={styles.symbolsBox}>
-                <Symbols />
-              </div>
-            </div>
-        </div>
-    </section>
+          </div>
+      </section>
+    </div>
   )
 }

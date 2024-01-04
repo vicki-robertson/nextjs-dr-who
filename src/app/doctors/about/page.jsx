77712008ac3787f951e9c5e52e80dd65524dd7f5
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-04 13:20:59 +0100

    changes to doctors page, forgot to change branch

--- src/app/doctors/about/page.jsx
+++ src/app/doctors/about/page.jsx
@@ -4,7 +4,7 @@ import Button from '../../components/Button/Button';
 import Image from 'next/image';
 
 export default function DoctorsAbout() {
-// Filter the doctorsList to only include the doctor with id 10
+// Filter the doctorsList to only include the doctor with id 14
 const selectedDoctor = doctorsList.doctors.find((doctor) => doctor.id === 14);
 
     return (
@@ -12,25 +12,25 @@ const selectedDoctor = doctorsList.doctors.find((doctor) => doctor.id === 14);
         {selectedDoctor && (
             <div key={selectedDoctor.id}>    
                 <div className={styles.gridBox}>
-                        <div className={styles.title}>
-                            <h2 >Conoce a ...{selectedDoctor.doctor.toLowerCase()}</h2>
-                        </div>
-                        <div className={styles.info}>
-                            <p>Mollit consectetur do qui magna consectetur sint est duis. Magna ullamco est quis laboris reprehenderit reprehenderit proident ex sunt voluptate ex exercitation. Sint incididunt irure in elit. Nulla est eu anim eiusmod nisi exercitation.</p>
-                        </div>
+                    <div className={styles.title}>
+                        <h2 >Conoce a ...{selectedDoctor.doctor.toLowerCase()}</h2>
+                    </div>
+                    <div className={styles.info}>
+                        <p>Mollit consectetur do qui magna consectetur sint est duis. Magna ullamco est quis laboris reprehenderit reprehenderit proident ex sunt voluptate ex exercitation. Sint incididunt irure in elit. Nulla est eu anim eiusmod nisi exercitation.</p>
                         <div className={styles.button}>
                             <Button href="https://www.doctorwho.tv/">
                                 Ver más
                             </Button>
                         </div>
+                    </div>
                     
                     <div class={styles.image}>
                         <Image className={styles.doctorPhoto}
                             src={selectedDoctor.image}
                             alt='fotos de los doctores'
-                            height={800}
-                            width={500}
-                            objectFit='cover'
+                            height={814}
+                            width={606}
+                            style={{objectFit:"cover"}}
                         />
                     </div>
                 </div>

ae0c7809b438d440929e489fddff9c941b5913c9
vicki-robertson <135024584+vicki-robertson@users.noreply.github.com>
2024-01-06 00:29:47 +0100

    symbols added to 'about' bkground

--- src/app/doctors/about/page.jsx
+++ src/app/doctors/about/page.jsx
@@ -1,7 +1,9 @@
 import styles from './page.module.scss';
+import Image from 'next/image';
+
 import doctorsList from '../../doctors.json';
 import Button from '../../components/Button/Button';
-import Image from 'next/image';
+import SymbolsAbout from '@/app/components/Symbols/SymbolsAbout';
 
 export default function DoctorsAbout() {
 // Filter the doctorsList to only include the doctor with id 14
@@ -14,6 +16,9 @@ const selectedDoctor = doctorsList.doctors.find((doctor) => doctor.id === 14);
                 <div className={styles.gridBox}>
                     <div className={styles.title}>
                         <h2 >Conoce a ...{selectedDoctor.doctor.toLowerCase()}</h2>
+                        <div className={styles.symbolBox}>
+                            <SymbolsAbout />
+                        </div>
                     </div>
                     <section className={styles.info}>
                         <p>Mollit consectetur do qui magna consectetur sint est duis. Magna ullamco est quis laboris reprehenderit reprehenderit proident ex sunt voluptate ex exercitation. Sint incididunt irure in elit. Nulla est eu anim eiusmod nisi exercitation.</p>
